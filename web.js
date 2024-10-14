7b4ead458d2d653f59760d2a74e98df371819310
unknown <fathirsadewa137@gmail.com>
2024-10-14 13:58:50 +0700

    first web

---
+++ web.js
@@ -0,0 +1,9 @@
+function redirInstagram() {
+    window.location.href = "https://instagram.com/paatirrrrr"
+}
+function redirWhatsapp() {
+    window.location.href = "https://wa.me/085888467876"
+}
+function redirFacebook(){
+    window.location.href = "https://www.facebook.com/Raihan.Alfathir"
+}
